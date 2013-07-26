9215d81fc0a376e8421f19512a985ff8ef75b039
gzoreslav@gmail.com <gzoreslav@gmail.com@52037790-2622-c92e-d8ce-f5577c25c665>
2013-07-26 11:19:02 +0000

    templating #1

---
+++ trunk/uc2/articles.php
@@ -0,0 +1,12 @@
+<?php
+
+    include('global.properties');
+
+    $title = $_title;
+    $mainmenuindex = 5;
+    $content_tmpl = 'short_content';
+    $maincontent_tmpl = 'contentarticles';
+
+    include('index.tmpl');
+
+?>
---
+++ trunk/uc2/delivery.php
@@ -0,0 +1,12 @@
+<?php
+
+    include('global.properties');
+
+    $title = $_title;
+    $mainmenuindex = 1;
+    $content_tmpl = 'short_content';
+    $maincontent_tmpl = 'contentdelivery';
+
+    include('index.tmpl');
+
+?>
