aeb440340713342c7e04d736c08172381bf2bfc7
gzoreslav@gmail.com <gzoreslav@gmail.com@52037790-2622-c92e-d8ce-f5577c25c665>
2013-07-30 11:23:32 +0000

    first admin niamka

---
+++ trunk/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+    include('global.properties');
+
+    $title = "SAUTO - Admin";
+    $mainmenuindex = 1;
+    $content_tmpl = 'admin_content';
+    $maincontent_tmpl = 'adminindex';
+
+    include('admin.tmpl');
+
+?>
