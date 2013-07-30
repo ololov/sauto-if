7d67b2a4a375381fa516ef6aa7799fc1cd83d0f3
gzoreslav@gmail.com <gzoreslav@gmail.com@52037790-2622-c92e-d8ce-f5577c25c665>
2013-07-30 16:07:11 +0000

    first admin niamka 2

---
+++ trunk/promoeditor.php
@@ -0,0 +1,12 @@
+<?php
+
+    include('global.properties');
+
+    $title = "SAUTO - Редагування промоцій";
+    $mainmenuindex = 3;
+    $content_tmpl = 'admin_content';
+    $maincontent_tmpl = 'promoeditor';
+
+    include('admin.tmpl');
+
+?>
