9fa853a1e277a4bcbf58a925497e027d1dcdd764
gzoreslav@gmail.com <gzoreslav@gmail.com@52037790-2622-c92e-d8ce-f5577c25c665>
2013-07-31 08:08:45 +0000

    articles try 1

--- trunk/articles.php
+++ trunk/articles.php
@@ -5,6 +5,11 @@
     $title = $_title;
     $mainmenuindex = 5;
     $content_tmpl = 'short_content';
+    $articles = file('articles/config/articleslist.properties');
+    $count = count($articles);
+    for ($i = 0; $i < $count; $i++) {
+        $articles[$i] = trim($articles[$i]);
+    }
     $maincontent_tmpl = 'contentarticles';
 
     include('index.tmpl');
--- trunk/index.php
+++ trunk/index.php
@@ -5,6 +5,11 @@
     $title = $_title;
     $mainmenuindex = 3;
     $content_tmpl = 'index_content';
+    $articles = file('articles/config/articleslist.properties');
+    $count = count($articles);
+    for ($i = 0; $i < $count; $i++) {
+        $articles[$i] = trim($articles[$i]);
+    }
     $maincontent_tmpl = 'contenthot';
 
     include('index.tmpl');
