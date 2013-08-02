cfd63c2aa791ace84034659a47fe89c4f2b1d47b
gzoreslav@gmail.com <gzoreslav@gmail.com@52037790-2622-c92e-d8ce-f5577c25c665>
2013-08-02 08:34:40 +0000

    updates

---
+++ trunk/catalog.php
@@ -0,0 +1,12 @@
+<?php
+
+    include('global.properties');
+
+    $title = $_title;
+    $mainmenuindex = 6;
+    $content_tmpl = 'short_content';
+    $maincontent_tmpl = 'contentcatalog';
+
+    include('index.tmpl');
+
+?>
