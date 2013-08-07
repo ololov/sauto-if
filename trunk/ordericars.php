754c25f343172755f97eac2865abb636cacdada8
gzoreslav@gmail.com <gzoreslav@gmail.com@52037790-2622-c92e-d8ce-f5577c25c665>
2013-08-07 17:13:44 +0000

    orders 2

---
+++ trunk/ordericars.php
@@ -0,0 +1,21 @@
+<?php
+
+    include('global.properties');
+    include('mysql.php');
+
+    $title = $_title;
+
+    $mainmenuindex = 20;
+    $order_index = 1;
+    $result = $_GET['result'];
+    $order_id = $_GET['order_id'];
+    $order_title = $_order_id[$order_index];
+    $returnurl = $_order_url[$order_index];
+    $formorder = 'formorder.tmpl';
+    $formorderdetails = 'formorderpartner.tmpl';
+    $content_tmpl = 'short_content';
+    $maincontent_tmpl = 'contentorder';
+
+    include('index.tmpl');
+
+?>
