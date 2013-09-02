a958bb74a10f6a45b1333f44d8ec2a71a8250544
gzoreslav@gmail.com <gzoreslav@gmail.com@52037790-2622-c92e-d8ce-f5577c25c665>
2013-09-02 21:35:13 +0000

    try_la_la33

--- trunk/validate.js
+++ trunk/validate.js
@@ -1,8 +1,28 @@
+    $.validator.addMethod("check_files", function(value) {
+    
+    var uploader = $('#uploader').plupload('getUploader');
+
+        // Files in queue upload them first
+        if (uploader.files.length > 0) {
+            // When all files are uploaded submit form
+            if (uploader.files.length === (uploader.total.uploaded + uploader.total.failed)) return true;
+            uploader.bind('StateChanged', function() {
+                if (uploader.files.length === (uploader.total.uploaded + uploader.total.failed)) {
+                    //$('form')[0].submit();
+                    $("#order").submit();
+                }
+            });
+            uploader.start();
+        } else
+            return false;
+
+        }, 'check files');
+
 $(document).ready(function() {
 						   
 	$("fieldset input").click(function(){$("fieldset label.error").remove()});
 	
-	$("#order").validate({
+	var validator = $("#order").validate({
 		rules: {
 			nickname: {
 				required: true,
@@ -15,7 +35,8 @@ $(document).ready(function() {
 			phone: {
 				required: true,
 				minlength: 5
-				}
+				},
+            uploader_count: "check_files"
 		},
 		messages:{
 			nickname:{
@@ -26,12 +47,14 @@ $(document).ready(function() {
 				required: "будь-ласка, введіть ваш email",
 				email: "email введений невірно",
 			},
-			phone: "будь-ласка, введіть правильний номер телефону"
+			phone: "будь-ласка, введіть правильний номер телефону",
+			uploader_count: "потрібно завантажити щонайменше 1 файл"
 		},
 		errorPlacement: function(error, element) {
-			if (element.attr("name") == "nickname") error.insertAfter($("input[name=nickname]"));
-			if (element.attr("name") == "phone") error.insertAfter($("input[name=phone]"));
-			if (element.attr("name") == "email") error.insertAfter($("input[name=email]"));
+			if (element.attr("name") == "nickname") error.insertBefore($("#orderNicknameBlock"));
+			if (element.attr("name") == "phone") error.insertBefore($("#orderPhoneBlock"));
+			if (element.attr("name") == "email") error.insertBefore($("#orderEmailBlock"));
+			if (element.attr("name") == "uploader_count") error.insertBefore($("#orderFile"));
 		}	
 	});
 })
