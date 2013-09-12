0c0910a8ac644537216f060bdccc09e2941ef5d6
gzoreslav@gmail.com <gzoreslav@gmail.com@52037790-2622-c92e-d8ce-f5577c25c665>
2013-09-12 16:23:06 +0000

    1235

--- trunk/newarticle.php
+++ trunk/newarticle.php
@@ -2,9 +2,13 @@
 
     $get_all = true;
     include('getarticles.php');
-    $current = file_get_contents('articles/config/articleslist.properties');
-    $new = 'article'.($count+1).'|Нова стаття|0'.PHP_EOL.$current;
-    file_put_contents('articles/config/articleslist.properties', $new);
+    $articlefile = 'article'.($count+1);
+    $propfile = 'articles/config/articleslist.properties';
+    $current = file_get_contents($propfile);
+    $new = $articlefile.'|Нова стаття '.($count+1).'|0'.PHP_EOL.$current;
+    file_put_contents($propfile, $new);
+    file_put_contents('articles/'.$articlefile.'.htm', 'Повний опис нової статті '.($count+1));
+    file_put_contents('articles/'.$articlefile.'_short.htm', 'Скорочений опис нової статті '.($count+1));
 
     header("Location: http://sauto.if.ua/articleseditor.php");
 
