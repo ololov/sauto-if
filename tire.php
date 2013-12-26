b17ae3461ceef1e7f8ee6e0f06f58bef70de1958
gzoreslav <gzoreslav@gmail.com>
2013-12-26 10:51:54 +0000

    aaa

---
+++ tire.php
@@ -0,0 +1,12 @@
+<?php
+
+    include('global.properties');
+
+    $title = $_title;
+    $mainmenuindex = 7;
+    $content_tmpl = 'short_content';
+    $maincontent_tmpl = 'tire_table';
+
+    include('index.tmpl');
+
+?>
